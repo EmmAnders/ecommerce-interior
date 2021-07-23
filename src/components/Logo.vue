e245041d76b3807543bb28a59cd9a61b79e4efd8
Emma <emmandersson166@gmail.com>
2021-07-24 00:58:08 +0200

    added logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="logo">
+    <router-link to="/">LO<br />GO</router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/scss/Variables.scss";
+@import "@/scss/Mixins.scss";
+
+.logo {
+  font-family: $font-secondary;
+  z-index: 101;
+
+  a {
+    color: $black;
+  }
+
+  @include desktop {
+    font-size: 2rem;
+  }
+}
+</style>
