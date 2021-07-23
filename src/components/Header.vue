7a60d4ce46e162da6dbe4b7b53d0f59a46b763c2
Emma <emmandersson166@gmail.com>
2021-07-24 00:54:51 +0200

    added sidebar menu + header

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <div class="flex">
+      <Logo />
+      <div class="flex">
+        <p>LOGIN</p>
+        <p>CART</p>
+      </div>
+    </div>
+    <SidebarMenu />
+  </header>
+</template>
+
+<script>
+import SidebarMenu from "./SidebarMenu";
+import Logo from "./Logo";
+
+export default {
+  components: {
+    SidebarMenu,
+    Logo,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  .flex {
+    display: flex;
+    justify-content: space-between;
+    height: 5vh;
+    z-index: 101;
+  }
+}
+</style>
---
+++ src/components/SidebarMenu.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="sidebar-container">
+    <Overlay :toggleSidebar="toggleSidebar" :activeSidebar="activeSidebar" />
+    <div v-bind:class="{ activeSidebar: activeSidebar }" class="sidebar">
+      <button
+        class="menu-btn"
+        v-show="windowWidth < 1000"
+        @click="toggleSidebar"
+      >
+        MENU
+      </button>
+      <NavLinks :toggleSidebar="toggleSidebar" />
+    </div>
+  </div>
+</template>
+
+<script>
+import NavLinks from "./NavLinks";
+import Overlay from "./Overlay";
+
+export default {
+  components: { NavLinks, Overlay },
+  data() {
+    return { windowWidth: window.innerWidth, activeSidebar: false };
+  },
+
+  methods: {
+    toggleSidebar() {
+      this.activeSidebar = !this.activeSidebar;
+    },
+  },
+
+  mounted() {
+    window.onresize = () => {
+      this.windowWidth = window.innerWidth;
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/Variables.scss";
+@import "@/scss/Mixins.scss";
+
+.sidebar {
+  background: $white;
+  font-size: $paragraph-mobile-tablet;
+  width: 60vw;
+  position: fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  padding: 1rem 0rem 1rem 1rem;
+  transition: 0.5s;
+  z-index: 102;
+
+  button {
+    background: none;
+    border: none;
+    padding: 0;
+    outline: inherit;
+  }
+
+  @include desktop {
+    font-size: $paragraph-desktop;
+    width: 20vw;
+  }
+
+  .menu-btn {
+    position: absolute;
+    top: 50%;
+    right: 0;
+    transform: rotate(-90deg);
+    color: $black;
+    cursor: pointer;
+  }
+}
+
+.sidebar.activeSidebar {
+  left: -50vw;
+  transition: 0.5s;
+
+  @include desktop {
+    left: 0;
+  }
+}
+</style>
