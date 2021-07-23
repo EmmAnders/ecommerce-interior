10bef9860bd9dba02294a3668acfde684acd3b96
Emma <emmandersson166@gmail.com>
2021-07-24 00:47:25 +0200

    -

--- api/index.js
+++ api/index.js
@@ -46,6 +46,31 @@ app.use(
 //Set Public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+/**  Add headers */
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:8080");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 //ROUTES
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/users", userRoutes);
