e9c184b0d890ff71d8e297015fbdea3dbba85093
Emma <emmandersson166@gmail.com>
2021-08-31 17:14:47 +0200

    responsive grid layout for products

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -2,26 +2,19 @@
   <div>
     <ProductFilter />
     <div class="products-container">
-      <div
-        class="product"
+      <ProductCard
         v-for="product in filteredProducts"
         :key="product._id"
-      >
-        <ProductCard
-          v-if="
-            product.category.name === $route.params.category ||
-              $route.params.category === 'all'
-          "
-          :product="product"
-          :image="productImages"
-        />
-      </div>
+        :product="product"
+        :image="productImages"
+        class="product"
+      />
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions, mapMutations } from "vuex";
+import { mapGetters, mapActions } from "vuex";
 
 import ProductCard from "../components/ProductCard";
 
@@ -51,19 +44,21 @@ export default {
           if (
             options.length !== 0 &&
             !options.includes(item[key]) &&
-            item.category.name == this.$route.params.category
+            item.category.name === this.$route.params.category
           ) {
             return false;
           }
         }
-        return true;
+        return (
+          item.category.name === this.$route.params.category ||
+          this.$route.params.category == "all"
+        );
       });
     },
   },
 
   methods: {
     ...mapActions("product", ["getProducts"]),
-    ...mapMutations("product", ["setFilter"]),
   },
 
   created() {
@@ -78,15 +73,16 @@ export default {
 
 .products-container {
   display: grid;
-  grid-gap: 1.5vw;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  grid-gap: calc(2em + 2vh) calc(1.5em + 1vmin);
+  grid-auto-flow: row dense;
 
   @include tablet {
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   }
 
   @include desktop {
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
   }
 
   .product {
